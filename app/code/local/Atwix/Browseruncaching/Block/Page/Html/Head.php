148fb873902404a0d3f770f6970d42d4f5633bb9
sickdaflip <philippbreitsprecher@gmail.com>
2020-03-12 10:24:07 +0100

    add modman and composer.json for better implementation

--- app/code/local/Atwix/Browseruncaching/Block/Page/Html/Head.php
+++ app/code/local/Atwix/Browseruncaching/Block/Page/Html/Head.php
@@ -21,8 +21,7 @@
 
 class Atwix_Browseruncaching_Block_Page_Html_Head extends Mage_Page_Block_Html_Head
 {
-    protected function _prepareStaticAndSkinElements($format, array $staticItems, array $skinItems,
-                                                      $mergeCallback = null)
+    protected function _prepareStaticAndSkinElements($format, array $staticItems, array $skinItems, $mergeCallback = null)
     {
         $html = parent::_prepareStaticAndSkinElements($format, $staticItems, $skinItems, $mergeCallback);
 
